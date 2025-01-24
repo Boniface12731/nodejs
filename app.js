ded1afd8bf173a05a799df03ff3ec8fed934fc20
Bonface Osuka <bonyosuks12731@gmail.com>
2025-01-24 09:55:55 +0300

    nodelearn

--- app.js
+++ app.js
@@ -1,108 +1,108 @@
-   const  fs  =  require('fs');
-   const  http = require('http');
-   const  path  = require('path');
-   const  os =  require('os');
-   const url = require('url');
-   const crypto = require('crypto');
-   const sayHello = require('./greetings');
-   const  math =  require('./math');
-   const lodash = require('lodash');
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const os = require('os');
+const url = require('url');
+const crypto = require('crypto');
+const sayHello = require('./greetings');
+const math = require('./math');
+const lodash = require('lodash');
 const { error } = require('console');
 
-    // fs.readFile('example.txt', 'utf8', (err, data) => {
-    // if(err) {
-    //     console.error(err);
-    //     return;
-    // }
-    // console.log(data);
-    // });
+// fs.readFile('example.txt', 'utf8', (err, data) => {
+// if(err) {
+//     console.error(err);
+//     return;
+// }
+// console.log(data);
+// });
 
-    // const content  = 'Hello  NodeJs'
-    // fs.writeFile('output.txt', content, (err) => {
-    //     if(err){
-    //        console.error(err);
-    //        return;
-    //     }
-    //     console.log('File written successfully');
-    // })
+// const content  = 'Hello  NodeJs'
+// fs.writeFile('output.txt', content, (err) => {
+//     if(err){
+//        console.error(err);
+//        return;
+//     }
+//     console.log('File written successfully');
+// })
 
-    // const server = http.createServer((req, res) => {
-    //     res.statusCode = 200;
-    //     res.setHeader('Content-Type', 'text/plain');
-    //     res.end('Hello World!');
-    // })
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World!');
+// })
 
-    // server.listen(3000, ()=>{
-    //     console.log('Server  is  running  on port 3000');
-    // })
+// server.listen(3000, ()=>{
+//     console.log('Server  is  running  on port 3000');
+// })
 
-    //  const  directory  =  '/user/local'
-    //  const  fileName =  'example.txt'
+//  const  directory  =  '/user/local'
+//  const  fileName =  'example.txt'
 
-    //  const  fullPath  =  path.join(directory, fileName); 
-    //  console.log(fullPath);
+//  const  fullPath  =  path.join(directory, fileName); 
+//  console.log(fullPath);
 
-    // console.log('Platform:', os.platform());
-    // console.log('CPU Architecture:', os.arch());
-    // console.log('Total Memory:', os.totalmem());
-    // console.log('Free  Memory:', os.freemem());
+// console.log('Platform:', os.platform());
+// console.log('CPU Architecture:', os.arch());
+// console.log('Total Memory:', os.totalmem());
+// console.log('Free  Memory:', os.freemem());
 
-   // const  myUrl = new URL('https://example.com:8080/path/name?query=hello#hash')
-   // console.log('Host', myUrl.host);
-   // console.log('PathName', myUrl.pathname);
-   // console.log('Search Params', myUrl.searchParams.get('query'));
+// const  myUrl = new URL('https://example.com:8080/path/name?query=hello#hash')
+// console.log('Host', myUrl.host);
+// console.log('PathName', myUrl.pathname);
+// console.log('Search Params', myUrl.searchParams.get('query'));
 
-   // const  hash = crypto.createHash('sha256');
-   // hash.update('Hello, world!')
-   // console.log(hash.digest('hex'));
+// const  hash = crypto.createHash('sha256');
+// hash.update('Hello, world!')
+// console.log(hash.digest('hex'));
 
-   // const  message  = sayHello('Developers');
-   // console.log(message);
-   // console.log(math.add(5,3));
-   // console.log(math.subtract(5,3));
+// const  message  = sayHello('Developers');
+// console.log(message);
+// console.log(math.add(5,3));
+// console.log(math.subtract(5,3));
 
-   // const  numbers = [1,2,3,4,5];
-   // const  reversed  = lodash.reverse(numbers);
-   // console.log(reversed);
+// const  numbers = [1,2,3,4,5];
+// const  reversed  = lodash.reverse(numbers);
+// console.log(reversed);
 
-   // const readableStream = fs.createReadStream('example.txt', {encoding: 'utf8'})
+// const readableStream = fs.createReadStream('example.txt', {encoding: 'utf8'})
 
-   // readableStream.on('data', (chunk) => {
-   //    console.log(chunk);
-   // })
+// readableStream.on('data', (chunk) => {
+//    console.log(chunk);
+// })
 
-   // readableStream.on('end', () => {
-   //    console.log('Finished reading the file');
-   // })
+// readableStream.on('end', () => {
+//    console.log('Finished reading the file');
+// })
 
-   // readableStream.on('error', (err) => {
-   //    console.error('Error', err);
-   // })
+// readableStream.on('error', (err) => {
+//    console.error('Error', err);
+// })
 
-   // const  writableStream = fs.createWriteStream('output2.txt')
-   // writableStream.write('Hello, ')
-   // writableStream.write('World')
-   // writableStream.end()
-   // writableStream.on('finish', () => {
-   //    console.log('Finished writing');
-   // })
+// const  writableStream = fs.createWriteStream('output2.txt')
+// writableStream.write('Hello, ')
+// writableStream.write('World')
+// writableStream.end()
+// writableStream.on('finish', () => {
+//    console.log('Finished writing');
+// })
 
-   // const readableStream = fs.createReadStream('example.txt');
-   // const writableStream = fs.createWriteStream('example-output.txt');
-   // readableStream.pipe(writableStream);
-   // writableStream.on('finish',() => {
-   //    console.log('File  Copied  Successfully');  
-   // })
+// const readableStream = fs.createReadStream('example.txt');
+// const writableStream = fs.createWriteStream('example-output.txt');
+// readableStream.pipe(writableStream);
+// writableStream.on('finish',() => {
+//    console.log('File  Copied  Successfully');  
+// })
 
-   // const readline  =  require('readline');
-   // const readableStream = fs.createReadStream('example.txt')
-   // const  rl = readline.createInterface({input:readableStream})
-   // rl.on('line', (line) => {
-   //    console.log('Line:', line);
-   // })
-   // rl.on('close', () =>{
-   //    console.log('Finished  Processing the  file');  
-   // })
+// const readline  =  require('readline');
+// const readableStream = fs.createReadStream('example.txt')
+// const  rl = readline.createInterface({input:readableStream})
+// rl.on('line', (line) => {
+//    console.log('Line:', line);
+// })
+// rl.on('close', () =>{
+//    console.log('Finished  Processing the  file');  
+// })
 
 // fs.mkdir('newDirectory', (err) => {
 //    if(err){
@@ -161,7 +161,32 @@ const { error } = require('console');
 //    }
 // })
 
+const EventEmitter = require('events');
+const emitter = new EventEmitter()
+
+//registering  an event  listener  
+
+emitter.on('test1', () => {
+   console.log('An event has  occured in test1');
+})
+
+emitter.on('error', (err) => {
+   console.error('Errror event:', err.message);
+})
+
+try {
+   emitter.emit('test1')
+   emitter.removeListener('test1')
+   emitter.emit('test1')
+} catch (error) {
+   emitter.emit('error', error)
+}
+
+emitter.on('test1', () => {
+   console.log('An event has  occured in test2');
+})
 
 
-   
+
+
 
--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const http = require('http');
+const url = require('url')
+const queryString = require('querystring')
+ 
 const server = http.createServer((req, res) => {
-    if(req.method === 'POST' && req.url === '/submit'){
-        let body = ''
-        req.on('data', (chunk) => {
-            body+= chunk.toString()
-        })
-        req.on('end', () => {
-            res.writeHead(200,{'content-type' : 'application/json'})
-            res.end(JSON.stringify({message: 'Data received', data:body}));
-        })
-    } 
-    else{
-       res.writeHead(404, {"content-type": 'text/plain'})
-       res.end('Route  not  found')
-    }
-})
+   if(req.method === 'POST' && req.url === '/submit'){
+    let data = ' '
 
-const PORT = 3000
-server.listen(PORT, () => {
-    console.log(`Server is running  on  localhost: ${PORT}`);
-})
+    req.on('data', (chunk)=> {
+        data += chunk
+    })
+    req.on('end', () => {
+        const parsedData = queryString.parse(data)
+        res.writeHead(200, {'content-type' : 'application/json'})
+        res.end(JSON.stringify({message: "Formdata  received", parsedData}))
+    })
+   }
+ })
+
+    const PORT = 3000
+    server.listen(PORT, () => {
+        console.log(`Server is running  on  localhost: ${PORT}`);
+    })
